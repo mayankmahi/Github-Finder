110930a80737b8d6389be9df072f4898a2bd7ec5
mayankmahi <mkmahi1198@gmail.com>
2021-05-22 12:34:52 +0530

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 
+// Components
+import Navbar from "./components/layout/Navbar";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Github Finder App</h1>
+        <Navbar />
       </div>
     );
   }
